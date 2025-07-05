7667a4ce1ecb2ffe85cc8bfee78f30d1fd9ece2a
Jakhongirbek <jakhonyokubjonov@gmail.com>
2025-07-05 10:27:25 +0900

    CORS

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,12 @@ const branchRoutes = require('./routes/branches')
 const app = express()
 
 // Middleware
-app.use(cors())
+app.use(
+	cors({
+		origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+		credentials: true,
+	})
+)
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
